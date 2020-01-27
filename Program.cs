f6b0be32bdd2a0b09f98d2b52b2ecccbe25a4ef4
Tomas Profeld <tomas.profeld@ape-electronic.cz>
2020-01-27 20:42:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squirrel;
+
+namespace MyApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static async Task Main()
+        {
+            using (var mgr = new UpdateManager("C:\\VSProject\\MyApp\\Releases"))
+            {
+                await mgr.UpdateApp();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ envelope.ps1
@@ -0,0 +1,5 @@
+cd bin/Release
+if (-NOT(test-path uncoded))
+ {mkdir uncoded}
+move MyApp.exe uncoded
+C:\"Program Files"\"Sentinel LDK"\VendorTools\VendorSuite\envelope.com  -p c:/VSProject/MyApp/MyApp.prjx
